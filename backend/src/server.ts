2214cba8412cebf645fbeb07322a4e37c6bb853c
Adir Noyman <ofahol2007@hotmail.com>
2026-02-03 16:28:12 +0200

    .

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,31 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import path from 'path';
+import { ENV } from './config/env.js';
 
 const app = express();
 
-// Check server is running
-app.get('/', (req: Request, res: Response) => {
-  res.send('Server is up and running! 🚀');
+// Get the absolute path to the current directory
+const __dirname = path.resolve();
+console.log("__dirname is 👉", __dirname)
+
+// Check health of the server in the api endpoint
+app.get('/api/health', (req: Request, res: Response) => {
+  res.status(200).json({ status: 'OK', message: 'Server is healthy! ✅' });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000 🤓🤘');
+if (ENV.NODE_ENV === 'production') {
+  // Serve our React application as static files
+  app.use(express.static(path.join(__dirname, '../admin/dist')));
+
+  app.get("/{*any}", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../admin/dist', 'index.html'));
+  });
+} else {
+  app.get('/', (req: Request, res: Response) => {
+    res.send('Server is running in development mode! 🚀');
+  });
+}
+app.listen(ENV.PORT, () => {
+  console.log(`Server is running on port ${ENV.PORT} 🤓🤘`);
 });
